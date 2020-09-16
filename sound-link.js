0496d9cc1a047af809543c14ec6321b8aee52be8
superseva <superseva@gmail.com>
2020-09-16 14:51:08 +0200

    Update sound-link.js

--- sound-link.js
+++ sound-link.js
@@ -1,5 +1,5 @@
 // LINK EXAMPLE
-// <a class="sound_link" data-playlist="Music" data-sound="music" data-loop="0">Play Music/music</a>
+// <a class="sound_link" data-playlist="Music" data-sound="music">My Sound</a>
 
 Hooks.on("renderItemSheet", (app, html, options) => {
             searchSoundLinks(html);
@@ -10,10 +10,8 @@ Hooks.on("renderJournalSheet", (app, html, options) => {
 });
 function searchSoundLinks (html){
     html.find('.sound_link').click((ev) => {
-                    let element = ev.currentTarget;                    
-					//let chatContent = `/toggle-playlist-sound ` + element.dataset.playlist + ` ` +  element.dataset.sound;
+                    let element = ev.currentTarget;        
 					let chatContent = `{{macro "toggle-playlist-sound" "` + element.dataset.playlist + `" "` +  element.dataset.sound + `"}}`;
-					//console.log(chatContent);
-					ChatMessage.create({content : chatContent});
+	    				ChatMessage.create({content : chatContent});
             });
 }
