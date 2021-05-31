be62e2c1cfa3e1c58d1c26ef6a2b8d756742b9e6
superseva <superseva@gmail.com>
2021-05-31 10:11:25 +0200

    to 8.x

--- sound-link.js
+++ sound-link.js
@@ -1,18 +1,28 @@
 // LINK EXAMPLE
-// <a class="sound_link" data-playlist="Music" data-sound="Song">My Song From the Music Playlist</a>
+// <a class="sound_link" data-playlist="Music" data-sound="music" data-loop="0">Play Music/music</a>
 
 Hooks.on("renderItemSheet", (app, html, options) => {
-            searchSoundLinks(html);
+    searchSoundLinks(html);
 });
 
 Hooks.on("renderJournalSheet", (app, html, options) => {
-            searchSoundLinks(html);
+    searchSoundLinks(html);
 });
-function searchSoundLinks (html){
-    html.find('.sound_link').click((ev) => {
-                    let element = ev.currentTarget;        
-					let chatContent = `{{macro "toggle-playlist-sound" "` + element.dataset.playlist + `" "` +  element.dataset.sound + `"}}`;
-	    				console.log(chatContent);
-	    				ChatMessage.create({content : chatContent});
-            });
+
+function searchSoundLinks(html) {
+    html.find(".sound_link").click((ev) => {
+        let element = ev.currentTarget;
+        let startsWith = '';
+        let playlistName = element.dataset.playlist;
+        let soundName = element.dataset.sound;
+
+        const playlist = game.playlists.contents.find(p => startsWith ? p.name.startsWith(playlistName) : p.name === playlistName);
+        if (!playlist)
+            return;
+        const sound = playlist.sounds.find(s => startsWith ? s.name.startsWith(soundName) : s.name === soundName);
+
+        if (sound)
+            playlist.updateEmbeddedDocuments("PlaylistSound", [{ _id: sound.id, playing: !sound.playing }]);
+
+    });
 }
